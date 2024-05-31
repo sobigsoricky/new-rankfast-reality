ef317545e8f20aa91f3081d7cc81e9be6d8df6dd
adityajain-web <87418360+sobigsoricky@users.noreply.github.com>
2024-05-31 18:27:05 +0530

    empty

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-// layout
-
-export { default as Header } from './layout/header'
-export { default as Footer } from './layout/footer'
-
-// cards
-
-export { default as TestimonialCard } from './cards/testimonial-card'
-export { default as IconBox } from './cards/icon-box'
--- src/data/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Clients } from './clients'
-export { default as Testimonial } from './client-testimonial'
-export { default as Solutions } from './solutions'
